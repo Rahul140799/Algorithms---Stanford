eca8f8b433fc6b328e959cccbda740a1b5d65f12
Ferran <ferran.9908@gmail.com>
2020-03-26 12:32:16 +0530

    Add quicksort.py quicksortInput.txt quick_select.py

---
+++ Divide and Conquer/quick_select.py
@@ -0,0 +1,25 @@
+import random
+
+def partition(nlist):
+    pivot_idx = random.randrange(len(nlist))
+    nlist[0],nlist[pivot_idx] = nlist[pivot_idx],nlist[0]
+    i = 1
+    for j in range(1,len(nlist)):
+        if nlist[j] < nlist[0]:
+            nlist[j],nlist[i] = nlist[i],nlist[j]
+            i+=1
+    nlist[i-1],nlist[0] = nlist[0],nlist[i-1]
+    return i-1,nlist
+
+def quick_select(nlist,i):
+    if len(nlist) == 0:
+        return nlist[0]
+    else:
+        pivot, nlist = partition(nlist)
+        if pivot == i:
+            return nlist[i]
+        elif pivot >  i:
+            return quick_select(nlist[:pivot],i)
+        else:
+
+            return quick_select(nlist[pivot+1:],i - pivot - 1)
---
+++ Divide and Conquer/quicksort.py
@@ -0,0 +1,50 @@
+
+count = 0
+
+def findPivotPosition(nlist):
+    new_list = list(enumerate(nlist))
+    if len(new_list) % 2 == 0:
+        final_list = [new_list[0],new_list[len(new_list)//2 - 1], new_list[len(new_list)-1]]
+    else:
+        final_list = [new_list[0],new_list[len(new_list)//2], new_list[len(new_list)-1]]
+
+    final_list.sort(key=lambda x:x[1])
+    return final_list[1][0]
+
+def partition(nlist):
+    i = 1
+    PivotPosition = findPivotPosition(nlist)
+    nlist[0],nlist[PivotPosition] = nlist[PivotPosition],nlist[0]
+    pivot = nlist[0]
+    for j in range(1,len(nlist)):
+        if nlist[j] < pivot:
+            nlist[i],nlist[j] = nlist[j],nlist[i]
+            i+=1
+    nlist[i-1],nlist[0] = nlist[0],nlist[i-1]
+    return i-1,nlist
+
+
+def quicksort(nlist):
+    global count
+    if len(nlist) <= 1:
+        return nlist
+    else:
+        pivot_index, partitioned_list = partition(nlist)
+        count+=len(partitioned_list[:pivot_index])
+        left = quicksort(partitioned_list[:pivot_index])
+        count+=len(partitioned_list[pivot_index+1:])
+        right = quicksort(partitioned_list[pivot_index+1:])
+        return left + [partitioned_list[pivot_index]] + right
+
+with open('quicksortInput.txt','r') as file:
+    nums = [int(i.strip())for i in file.readlines()]
+
+p = quicksort(nums)
+if p == sorted(nums):
+    print("sorted")
+    print(count)
+else:
+    print("Not sorted")
+
+
+
